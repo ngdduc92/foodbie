b7bbed72c80507070e176257aad5d446eeebb06b
FIRST_NAME LAST_NAME <lelong236610@gmail.com>
2023-06-13 00:20:23 +0700

    FB-4: Update Favorite page

--- components/header-option/index.tsx
+++ components/header-option/index.tsx
@@ -4,7 +4,7 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'next/router';
 
 export default function HeaderOption(props: any) {
-  const { title, link } = props;
+  const { title, link, isArrow } = props;
   const router = useRouter();
 
   const back = () => {
@@ -13,14 +13,14 @@ export default function HeaderOption(props: any) {
 
   return (
     <div className="d-flex justify-content-center align-items-center py-3 position-relative bg-white small__line">
-      <i className="position-absolute start-0 text-primary fs-1 ms-3">
+      {isArrow ? '' : <i className="position-absolute start-0 text-primary fs-1 ms-3" >
         <FontAwesomeIcon
           icon={faArrowLeft}
           onClick={() => {
             back();
           }}
         />
-      </i>
+      </i>}
       <p className="fs-2 fw-bold mb-0">{title}</p>
     </div>
   );
--- pages/favorite-store/index.tsx
+++ pages/favorite-store/index.tsx
@@ -1,75 +1,167 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './favorite-store.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import { faCircleCheck, faStar, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck, faStar, faHeart, faCheck } from '@fortawesome/free-solid-svg-icons';
 import Footer from '@/components/footer/footer.component';
+import HeaderOption from '@/components/header-option';
+import { Tabs, Tab, Button } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { uuid } from '@/components/utils';
 
 const cx = classNames.bind(styles);
 
+
+
 export default function FavoriteStore() {
+  const [dropdownBtnTitle, setDropdownBtnTitle] = useState('All Services')
+  const dataDropdown = ['All Services', 'Food', 'Fresh', 'Beer', 'Flowers', 'Mart']
   return (
     <>
       <div className={cx('wapper__favorite')}>
-        <div className={cx('favorite__content')}>
-          <div className={cx('favorite__content__item')}>
-            <div className={cx('item__right')}>
-              <img src="assets/images/maycha.jpg" />
+
+        <HeaderOption title={'Likes'} isArrow />
+        <Tabs
+          defaultActiveKey="Latest"
+          id="fill-tab-example"
+          className="mb-3 border-0"
+          fill
+        >
+          <Tab eventKey="Latest" title="Latest">
+            <DropdownButton variant="ouline-primary" id="dropdown-basic-button" title={dropdownBtnTitle} className="border-0 bg-light w-100 shadow-0">
+              {dataDropdown.map((item) => (
+                <Dropdown.Item key={uuid()} onClick={() => setDropdownBtnTitle(item)} className="d-flex justify-content-between w-100">
+                  {item}
+                  {dropdownBtnTitle === item && <FontAwesomeIcon icon={faCheck} className="text-primary" />}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+            <div className={cx('favorite__content')}>
+              <div className={cx('favorite__content__item')}>
+                <div className={cx('item__right')}>
+                  <img src="assets/images/maycha.jpg" />
+                </div>
+                <div className={cx('item__left')}>
+                  <span className={cx('title__item__left')}>
+                    <i>
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </i>
+                    MayCha Milk Tea
+                  </span>
+                  <div className={cx('evaluate')}>
+                    <i>
+                      <FontAwesomeIcon icon={faStar} />
+                    </i>
+                    <span>4.0 | 6.7km | 37min</span>
+                  </div>
+                  <div className={cx('promotion')}>
+                    <span>FE CREDIT 40K off</span>
+                  </div>
+                  <div className={cx('heart')}>
+                    <i>
+                      <FontAwesomeIcon icon={faHeart} />
+                    </i>
+                  </div>
+                </div>
+              </div>
+              <div className={cx('favorite__content__item')}>
+                <div className={cx('item__right')}>
+                  <img src="assets/images/maika.jpg" />
+                </div>
+                <div className={cx('item__left')}>
+                  <span className={cx('title__item__left')}>
+                    <i>
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </i>
+                    Maika Milk Tea
+                  </span>
+                  <div className={cx('evaluate')}>
+                    <i>
+                      <FontAwesomeIcon icon={faStar} />
+                    </i>
+                    <span>4.8 | 6.7km | 37km</span>
+                  </div>
+                  <div className={cx('promotion')}>
+                    <span> 15% off menu</span>
+                  </div>
+                  <div className={cx('heart')}>
+                    <i>
+                      <FontAwesomeIcon icon={faHeart} />
+                    </i>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className={cx('item__left')}>
-              <span className={cx('title__item__left')}>
-                <i>
-                  <FontAwesomeIcon icon={faCircleCheck} />
-                </i>
-                MayCha Milk Tea
-              </span>
-              <div className={cx('evaluate')}>
-                <i>
-                  <FontAwesomeIcon icon={faStar} />
-                </i>
-                <span>4.0</span>
-                <span>{'(15+) • 0.1km'}</span>
+          </Tab>
+          <Tab eventKey="Nearby" title="Nearby">
+            <DropdownButton variant="ouline-primary" id="dropdown-basic-button" title={dropdownBtnTitle} className="border-0 shadow-0 bg-light">
+              {dataDropdown.map((item) => (
+                <Dropdown.Item key={uuid()} onClick={() => setDropdownBtnTitle(item)} className="d-flex justify-content-between">
+                  {item}
+                  {dropdownBtnTitle === item && <FontAwesomeIcon icon={faCheck} className='primary' />}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+            <div className={cx('favorite__content')}>
+              <div className={cx('favorite__content__item')}>
+                <div className={cx('item__right')}>
+                  <img src="assets/images/maika.jpg" />
+                </div>
+                <div className={cx('item__left')}>
+                  <span className={cx('title__item__left')}>
+                    <i>
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </i>
+                    Maika Milk Tea
+                  </span>
+                  <div className={cx('evaluate')}>
+                    <i>
+                      <FontAwesomeIcon icon={faStar} />
+                    </i>
+                    <span>4.8 | 6.7km | 37km</span>
+                  </div>
+                  <div className={cx('promotion')}>
+                    <span> 15% off menu</span>
+                  </div>
+                  <div className={cx('heart')}>
+                    <i>
+                      <FontAwesomeIcon icon={faHeart} />
+                    </i>
+                  </div>
+                </div>
               </div>
-              <div className={cx('promotion')}>
-                <span>15% off menu</span>
+              <div className={cx('favorite__content__item')}>
+                <div className={cx('item__right')}>
+                  <img src="assets/images/maycha.jpg" />
+                </div>
+                <div className={cx('item__left')}>
+                  <span className={cx('title__item__left')}>
+                    <i>
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </i>
+                    MayCha Milk Tea
+                  </span>
+                  <div className={cx('evaluate')}>
+                    <i>
+                      <FontAwesomeIcon icon={faStar} />
+                    </i>
+                    <span>4.0 | 6.7km | 37min</span>
+                  </div>
+                  <div className={cx('promotion')}>
+                    <span>FE CREDIT 40K off</span>
+                  </div>
+                  <div className={cx('heart')}>
+                    <i>
+                      <FontAwesomeIcon icon={faHeart} />
+                    </i>
+                  </div>
+                </div>
               </div>
-              <div className={cx('heart')}>
-                <i>
-                  <FontAwesomeIcon icon={faHeart} />
-                </i>
-              </div>
+
             </div>
-          </div>
-          <div className={cx('favorite__content__item')}>
-            <div className={cx('item__right')}>
-              <img src="assets/images/maika.jpg" />
-            </div>
-            <div className={cx('item__left')}>
-              <span className={cx('title__item__left')}>
-                <i>
-                  <FontAwesomeIcon icon={faCircleCheck} />
-                </i>
-                Maika Milk Tea
-              </span>
-              <div className={cx('evaluate')}>
-                <i>
-                  <FontAwesomeIcon icon={faStar} />
-                </i>
-                <span>4.0</span>
-                <span>{'(15+) • 0.1km'}</span>
-              </div>
-              <div className={cx('promotion')}>
-                <span> 15% off menu</span>
-              </div>
-              <div className={cx('heart')}>
-                <i>
-                  <FontAwesomeIcon icon={faHeart} />
-                </i>
-              </div>
-            </div>
-          </div>
-        </div>
+          </Tab>
+        </Tabs>
       </div>
       <Footer />
     </>
