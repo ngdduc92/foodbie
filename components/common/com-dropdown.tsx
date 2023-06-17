3bfb06adf9a67df13ec6ea6d2747f102856d8662
Nguyễn Sanh Trưng <nstrung163@gmail.com>
2023-06-17 12:01:06 +0000

    Merged in bugfix/build-failed (pull request #19)
    Add type props to fix build failed
    
    * Add type props to fix build failed
    
    
    Approved-by: Duc Nguyen Dinh

--- components/common/com-dropdown.tsx
+++ components/common/com-dropdown.tsx
@@ -7,91 +7,110 @@ import { uuid } from '../utils';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-export const CVariantsDropDown = ({
-    variant,
-    title,
-    options,
-    isDivider,
-    dropdownButtonClasse,
-    menuClasse,
-    itemClass,
-    children,
-    active,
-    onClick
-}) => {
-    const { t } = useTranslation(NS_COMMON);
-    const [id] = useState(uuid());
+interface CVariantsDropDownProps {
+  variant: string;
+  title: string;
+  options: any[];
+  isDivider: boolean;
+  dropdownButtonClasse: string;
+  menuClasse: string;
+  itemClass: string;
+  children: any;
+  active: string | number;
+  onClick: Function;
+}
 
-    return (
-        <DropdownButton
-            key={uuid()}
-            id={'dropdown-variants'}
-            variant={variant.toLowerCase()}
-            title={title}
-            className={dropdownButtonClasse}
-        >
-            <Dropdown.Menu className={menuClasse}>
-                {options.map(data => {
-                    return (
-                        <div key={uuid()}>
-                            <Dropdown.Item
-                                className={itemClass}
-                                onClick={() => onClick(data)}
-                            >
-                                {data.value}
-                                {data.active && <FontAwesomeIcon icon={faCheck} className="text-primary" />}
-                            </Dropdown.Item>
-                            {isDivider && <Dropdown.Divider />}
-                        </div>)
-                })}
-            </Dropdown.Menu>
-        </DropdownButton>
-    );
+export const CVariantsDropDown = ({
+  variant,
+  title,
+  options,
+  isDivider,
+  dropdownButtonClasse,
+  menuClasse,
+  itemClass,
+  children,
+  active,
+  onClick,
+}: CVariantsDropDownProps) => {
+  const { t } = useTranslation(NS_COMMON);
+  const [id] = useState(uuid());
+
+  return (
+    <DropdownButton
+      key={uuid()}
+      id={'dropdown-variants'}
+      variant={variant.toLowerCase()}
+      title={title}
+      className={dropdownButtonClasse}
+    >
+      <Dropdown.Menu className={menuClasse}>
+        {options.map((data) => {
+          return (
+            <div key={uuid()}>
+              <Dropdown.Item className={itemClass} onClick={() => onClick(data)}>
+                {data.value}
+                {data.active && <FontAwesomeIcon icon={faCheck} className="text-primary" />}
+              </Dropdown.Item>
+              {isDivider && <Dropdown.Divider />}
+            </div>
+          );
+        })}
+      </Dropdown.Menu>
+    </DropdownButton>
+  );
 };
 
+interface CSplitDropDownProps {
+  variant: string;
+  titleButton: string;
+  titleToggle: string;
+  options: any[];
+  isDivider: boolean;
+  dropdownButtonClasse: string;
+  menuClasse: string;
+  itemClass: string;
+  children: any;
+  toggleClasse: string;
+  active: string | number;
+  onClick: Function;
+}
+
 export const CSplitDropDown = ({
-    variant,
-    titleButton,
-    titleToggle,
-    options,
-    isDivider,
-    dropdownButtonClasse,
-    menuClasse,
-    itemClass,
-    children,
-    toggleClasse,
-    active,
-    onClick
-}) => {
-    const { t } = useTranslation(NS_COMMON);
-    const [id] = useState(uuid());
+  variant,
+  titleButton,
+  titleToggle,
+  options,
+  isDivider,
+  dropdownButtonClasse,
+  menuClasse,
+  itemClass,
+  children,
+  toggleClasse,
+  active,
+  onClick,
+}: CSplitDropDownProps) => {
+  const { t } = useTranslation(NS_COMMON);
+  const [id] = useState(uuid());
 
-    return (
-        <Dropdown
-            key={uuid()}
-            id="dropdown-split"
-            className={dropdownButtonClasse}
-        >
-            {titleButton && <Button variant={variant.toLowerCase()}>{titleButton}</Button>}
-            <Dropdown.Toggle split variant={variant.toLowerCase()} className={toggleClasse}>
-                {titleToggle}
-            </Dropdown.Toggle>
-            <Dropdown.Menu className={menuClasse}>
-                {options.map(data => {
-                    return (
-                        <div key={uuid()}>
-                            <Dropdown.Item
-
-                                className={itemClass}
-                                onClick={() => onClick(data)}
-                            >
-                                {data.value}
-                                {data.active && <FontAwesomeIcon icon={faCheck} className="text-primary" />}
-                            </Dropdown.Item>
-                            {isDivider && <Dropdown.Divider />}
-                        </div>)
-                })}
-            </Dropdown.Menu>
-        </Dropdown>
-    );
+  return (
+    <Dropdown key={uuid()} id="dropdown-split" className={dropdownButtonClasse}>
+      {titleButton && <Button variant={variant.toLowerCase()}>{titleButton}</Button>}
+      <Dropdown.Toggle split variant={variant.toLowerCase()} className={toggleClasse}>
+        {titleToggle}
+      </Dropdown.Toggle>
+      <Dropdown.Menu className={menuClasse}>
+        {options.map((data) => {
+          return (
+            <div key={uuid()}>
+              <Dropdown.Item className={itemClass} onClick={() => onClick(data)}>
+                {data.value}
+                {data.active && <FontAwesomeIcon icon={faCheck} className="text-primary" />}
+              </Dropdown.Item>
+              {isDivider && <Dropdown.Divider />}
+            </div>
+          );
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 };
